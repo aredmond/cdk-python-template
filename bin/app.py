8b7d018d1cae83a70a9af1dfa4c564492b5c76cd
Redmond <anredmo@f018985fa189.ant.amazon.com>
2019-09-16 12:21:19 -0500

    Changed naming to be more generic

--- bin/app.py
+++ bin/app.py
@@ -8,11 +8,11 @@ from lib.example_stepfunction_stack import ExampleStepFunctionStack
 # Create an environment parameter by pulling relavent env info from system
 # variables, set defaults to ensure these variables are set before allowing
 # CDK to deploy the stack.
-nse_sbx_env_west_2 = {
+env_west_2 = {
     'account': environ.get('CDK_DEFAULT_ACCOUNT', '111111111111'),
     'region': environ.get('CDK_DEFAULT_REGION','us-west-2') }
 
 app = core.App()
-ExampleStepFunctionStack(app, "example-stepfunction-sbx", env=nse_sbx_env_west_2)
+ExampleStepFunctionStack(app, "example-stepfunction-sbx", env=env_west_2)
 
 app.synth()
